ace2ebe4962105dcd5e46ebb445534fbbe51fb76
Renê <renejr.arraes286@gmail.com>
2021-09-24 14:24:08 -0300

    create the first endpoint

--- src/main/kotlin/com/rene/VertxKotlin/MainVerticle.kt
+++ src/main/kotlin/com/rene/VertxKotlin/MainVerticle.kt
@@ -4,6 +4,7 @@ import com.rene.VertxKotlin.models.DevUser
 import io.vertx.core.json.Json
 import io.vertx.core.AbstractVerticle
 import io.vertx.ext.web.Router
+import io.vertx.ext.web.handler.BodyHandler
 import io.vertx.core.Promise
 
 class MainVerticle : AbstractVerticle() {
@@ -14,34 +15,18 @@ class MainVerticle : AbstractVerticle() {
 
 	override fun start(startPromise: Promise<Void>) {
 
-		var router: Router = Router.router(vertx)
-	
-				
-		vertx
-			.createHttpServer()
-			.requestHandler { req ->
-				req.response()
-					.putHeader("content-type", "application/json")
-					.end(Json.encodePrettily(devUser1))
-			}
-			.listen(8888) { http ->
-				if (http.succeeded()) {
-					startPromise.complete()
-					println("HTTP server started on port 8888")
-				} else {
-					startPromise.fail(http.cause());
-				}
-			}
+		val router: Router = Router.router(vertx)
+		router.route().handler(BodyHandler.create());
 
-
-
-		router.get("/users").handler { req ->
-
-
-			req.response().putHeader("content-type", "application/json").end(Json.encodePrettily(devUser1))
-			
-			
-
+		
+        router.get("/users").handler{req ->
+		
+			req.response().putHeader("content-type", "application/json").end(Json.encode(devUser1))
+		
 		}
+		
+		
+		vertx.createHttpServer().requestHandler(router).listen(8888)
+		
 	}
 }
